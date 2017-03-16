09f285e3a4dcdabf441d920f16518255eae24e2e
ANITA KHEDKAR <anita.khedkar@gmail.com>
2017-03-16 15:11:23 -0600

    author

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser')
 const recipe = require('./routes/recipe')
 const review = require('./routes/review')
 const step = require('./routes/step')
+const ingredient = require('./routes/ingredient')
+const author = require('./routes/author')
+const ingredient_recipe = require('./routes/ingredient_recipe')
 
 var cors = require('cors')
 app.use(cors())
@@ -14,6 +17,10 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use('/recipe', recipe)
 app.use('/review', review)
 app.use('/step', step)
+app.use('/ingredient', ingredient)
+app.use('/ingredient_recipe', ingredient_recipe)
+app.use('/author', author)
+
 
 app.listen(PORT, ()=> {
   console.log(`Listening on port ${PORT}`)
